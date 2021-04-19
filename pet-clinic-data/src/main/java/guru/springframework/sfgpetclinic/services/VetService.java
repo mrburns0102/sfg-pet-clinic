1973e6fb94b28876d05ccf34131ca2ea6ae9693e
dragan.dosen <dragan.dosen@seavus.com>
2021-04-19 22:03:05 +0200

    closes #13

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgpetclinic.services;
 
 import guru.springframework.sfgpetclinic.model.Vet;
+import guru.springframework.sfgpetclinic.services.map.AbstractMapService;
 
-public interface VetService extends CrudService<Vet, Long>{
+public interface VetService extends CrudService<Vet, Long> {
 }
